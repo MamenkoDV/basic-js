bbec7f9b4360e17f66e00457005ed4a0000f402e
MamenkoDV <mamenko.dv@gmail.com>
2020-09-29 02:57:39 +0200

    feat: dream-team task  accomplish

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(members) {
+  if (!(members instanceof Array)) return false;
+  return members
+    .filter((element) => typeof element === "string")
+    .map((element) => element.trim())
+    .filter((element) => Boolean(element))
+    .map((element) => element[0].toUpperCase())
+    .sort()
+    .join("");
   // remove line with error and write your code here
 };
